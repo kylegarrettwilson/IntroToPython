dbf6a8e8109629f364148906bc592d66097a198c
kylegarrettwilson <LetItBe@c-67-201-245-88.reshall.wwu.edu>
2015-10-31 19:00:24 -0700

    int(raw_input)
    raw input makes it a string which is why it won’t work on the function
    so you have to change it to a #

--- Madlib/main.py
+++ Madlib/main.py
@@ -6,7 +6,7 @@ tossed_name = raw_input("Enter a verb ")
 
 toppings_count = raw_input("Enter a number ")
 
-free_count = raw_input("Enter a number ")
+free_count = int(raw_input("Enter a number "))
 
 customer_count = raw_input("Enter a number ")
 
@@ -28,12 +28,15 @@ else:
     print "You must be new around here, give us a good tip."
 
 
-def pizza_count(x, y):
+def pizzaCount(x, y):
     coupon = x - y
     return coupon
 
-a = pizza_count(free_count, 4)
+a = pizzaCount(free_count, 30)
 print "You get " + str(a) + " free pizza coupons!"
+
+
+
 
 
 
