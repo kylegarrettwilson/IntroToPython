20770224ec0447d34b39df40ab4fca8483698029
kylegarrettwilson <LetItBe@c-67-201-245-88.reshall.wwu.edu>
2015-10-31 18:44:50 -0700

    defining story
    I am trying to write the if else and function statements so they make
    sense

--- Madlib/main.py
+++ Madlib/main.py
@@ -11,6 +11,31 @@ free_count = raw_input("Enter a number ")
 customer_count = raw_input("Enter a number ")
 
 
-""
+print "Hello and welcome to ", parlor_name
+
+
+if toppings_count <= 5:
+    supply = "ALL"
+    print "You get " + supply + " the topping you want!"
+else:
+    print "You don't get any toppings"
+
+
+if free_count > 80:
+    returning = "FREQUENT"
+    print "You are a " + returning + " customer so your pizza is free!"
+else:
+    print "You must be new around here, give us a good tip."
+
+
+def free_count(x):
+    coupon = x / 4
+    return coupon
+
+a = free_count()
+print "You get " + str(a) + " free pizza coupons!"
+
+
+
 
 
