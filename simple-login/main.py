fae37d12d428f905d3a24ddcd8896c066d22d2c6
kylegarrettwilson <LetItBe@c-67-201-245-114.reshall.wwu.edu>
2015-11-11 20:11:11 -0800

    Set 2
    This is setting up the variables as well as the html

--- simple-login/main.py
+++ simple-login/main.py
@@ -1,9 +1,37 @@
+'''
+Kyle Wilson
+11/9/15
+Design Patterns
+Simple Form Login
+'''
+
+
+
+
 
 import webapp2   # using web app as a browser
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        page = '''<!DOCTYPE HTML>
+<html>
+    <head>
+    <title>Login!</title>
+    </head>
+    <body>
+        <form method="GET">
+            <label>First Name: </label><input type="text" name="first" />
+            <label>Last Name: </label><input type="text" name="last" />
+            <label>Email: </label><input type="text" name="email" />
+            <input type="submit: value="Submit" />
+        </form>
+    </body>
+</html>'''
+        if self.request.GET:  #if get does exist then print the material
+            first = self.request.GET['first']    #This is storing form info within a variable and then send it out
+            last = self.request.GET['last']       #This is storing form info within a variable and then send it out
+            email = self.request.GET['email']    #This is storing form info within a variable and then send it out
+        self.response.write(page)              #this prints info onto the page, this is publish
 
 
 
