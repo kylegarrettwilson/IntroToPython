5269467d6132a2ac8d12b67caffe79da68511dd6
kylegarrettwilson <LetItBe@c-67-201-245-114.reshall.wwu.edu>
2015-11-11 20:21:40 -0800

    set 3
    This is using the get method to send more info and dividing the HTML
    page

--- simple-login/main.py
+++ simple-login/main.py
@@ -13,17 +13,24 @@ import webapp2   # using web app as a browser
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        page = '''<!DOCTYPE HTML>
+        page_head = '''<!DOCTYPE HTML>
 <html>
     <head>
     <title>Login!</title>
     </head>
-    <body>
-        <form method="GET">
+    <body>'''
+
+
+
+
+
+        page_body = '''<form method="GET">
             <label>First Name: </label><input type="text" name="first" />
             <label>Last Name: </label><input type="text" name="last" />
             <label>Email: </label><input type="text" name="email" />
-            <input type="submit: value="Submit" />
+            <input type="submit: value="Submit" />'''
+
+        page_close = '''
         </form>
     </body>
 </html>'''
@@ -31,7 +38,9 @@ class MainHandler(webapp2.RequestHandler):
             first = self.request.GET['first']    #This is storing form info within a variable and then send it out
             last = self.request.GET['last']       #This is storing form info within a variable and then send it out
             email = self.request.GET['email']    #This is storing form info within a variable and then send it out
-        self.response.write(page)              #this prints info onto the page, this is publish
+            self.response.write(page_head + user + ' ' + email + page_body + page_close)
+        else:
+            self.response.write(page_head + page_body + page_close)              #this prints info onto the page, this is publish
 
 
 
